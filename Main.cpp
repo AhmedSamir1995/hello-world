dd24bd382807bbd03be645481e571bd841298f39
SAmir <Samir@ahmeds-macbook-pro.local>
2016-03-19 16:47:14 +0200

    Addition

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+//
+//  Main.cpp
+//  Hello
+//
+//  Created by Ahmed Samir  on 3/19/16.
+//  Copyright © 2016 Ahmed Samir. All rights reserved.
+//
+
+#include "Main.hpp"
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "WElcome\n'";
+    return 0;
+}
---
+++ Main.hpp
@@ -0,0 +1,14 @@
+//
+//  Main.hpp
+//  Hello
+//
+//  Created by Ahmed Samir  on 3/19/16.
+//  Copyright © 2016 Ahmed Samir. All rights reserved.
+//
+
+#ifndef Main_hpp
+#define Main_hpp
+
+#include <stdio.h>
+
+#endif /* Main_hpp */
